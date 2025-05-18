6a6db1fb4403295a31d3cb900b9a56125878fc5e
BibasYonghang <bibasyonghang100@gmail.com>
2025-05-18 10:16:08 +0545

    Add GitHub Pages deploy workflow

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -7,64 +7,64 @@ let msg = document.querySelector('#msg');
 let turnO = true;
 
 const winPattern = [
-  [0, 1, 2],
-  [0, 3, 6],
-  [0, 4, 8],
-  [1, 4, 7],
-  [2, 5, 8],
-  [2, 4, 6],
-  [3, 4, 5],
-  [6, 7, 8],
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
 ];
 
 const resetGame = () => {
-  turnO = true;
-  enableBoxes();
-  msgContainer.classList.add("hide");
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
 };
 
 const disableBoxes = () => {
-  boxes.forEach((box) => {
-    box.disabled = true;
-  });
+    boxes.forEach((box) => {
+        box.disabled = true;
+    });
 };
 
 const enableBoxes = () => {
-  boxes.forEach((box) => {
-    box.disabled = false;
-    box.innerText = "";
-  });
+    boxes.forEach((box) => {
+        box.disabled = false;
+        box.innerText = "";
+    });
 };
 
 const showWinner = (winner) => {
-  msg.innerText = `Congratulations, Winner is ${winner}`;
-  msgContainer.classList.remove("hide");
-  disableBoxes();
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
 };
 
 const checkWinner = () => {
-  for (let pattern of winPattern) {
-    let pos1 = boxes[pattern[0]].innerText;
-    let pos2 = boxes[pattern[1]].innerText;
-    let pos3 = boxes[pattern[2]].innerText;
+    for (let pattern of winPattern) {
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
 
-    if (pos1 !== "" && pos1 === pos2 && pos2 === pos3) {
-      showWinner(pos1);
-      return;
+        if (pos1 !== "" && pos1 === pos2 && pos2 === pos3) {
+            showWinner(pos1);
+            return;
+        }
     }
-  }
 };
 
 boxes.forEach((box) => {
-  box.addEventListener("click", () => {
-    if (box.innerText !== "") return;
+    box.addEventListener("click", () => {
+        if (box.innerText !== "") return;
 
-    box.innerText = turnO ? "O" : "X";
-    box.disabled = true;
-    turnO = !turnO;
+        box.innerText = turnO ? "O" : "X";
+        box.disabled = true;
+        turnO = !turnO;
 
-    checkWinner();
-  });
+        checkWinner();
+    });
 });
 
 resetButton.addEventListener("click", resetGame);
